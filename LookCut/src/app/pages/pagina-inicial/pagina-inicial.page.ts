cca9f600fe515c85123b7d351c0198e840a30328
juanDavidVargas <jdvargas752@misena.edu.co>
2020-07-22 20:19:53 -0500

    Ajustes al diseño del formulario de registro

--- LookCut/src/app/pages/pagina-inicial/pagina-inicial.page.ts
+++ LookCut/src/app/pages/pagina-inicial/pagina-inicial.page.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-pagina-inicial',
-  templateUrl: './pagina-inicial.page.html',
-  styleUrls: ['./pagina-inicial.page.scss'],
+  selector: "app-pagina-inicial",
+  templateUrl: "./pagina-inicial.page.html",
+  styleUrls: ["./pagina-inicial.page.scss"],
 })
 export class PaginaInicialPage implements OnInit {
+  logo = [
+    {
+      image: "assets/imgs/Logo_LookCut.jpeg",
+      title: "LookCut",
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
